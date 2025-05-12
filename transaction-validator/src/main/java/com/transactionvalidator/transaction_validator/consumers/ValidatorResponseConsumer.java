384c8b55898727a94add476ed5af5c7d7b02e28e
rrs6 <rrs6@cin.ufpe.br>
2025-05-12 16:13:48 -0300

    feat: last commit

---
+++ transaction-validator/src/main/java/com/transactionvalidator/transaction_validator/consumers/ValidatorResponseConsumer.java
@@ -0,0 +1,23 @@
+package com.transactionvalidator.transaction_validator.consumers;
+
+import org.springframework.stereotype.Component;
+
+import com.transactionvalidator.transaction_validator.dtos.ValidatorResponse;
+import com.transactionvalidator.transaction_validator.services.PaymentService;
+
+import io.awspring.cloud.sqs.annotation.SqsListener;
+
+@Component
+public class ValidatorResponseConsumer {
+
+    private final PaymentService paymentService;
+
+    public ValidatorResponseConsumer(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+    
+    @SqsListener("processed-payment")
+    public void listen(ValidatorResponse response) {
+        paymentService.updatePaymentByValidatorResponse(response);
+    }
+}
--- transaction-validator/src/main/java/com/transactionvalidator/transaction_validator/services/PaymentService.java
+++ transaction-validator/src/main/java/com/transactionvalidator/transaction_validator/services/PaymentService.java
@@ -1,6 +1,6 @@
 package com.transactionvalidator.transaction_validator.services;
 
-import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,37 +8,41 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.transactionvalidator.transaction_validator.db.entities.Payment;
 import com.transactionvalidator.transaction_validator.db.repositories.PaymentRepository;
-import com.transactionvalidator.transaction_validator.dtos.PaymentRecord;
+import com.transactionvalidator.transaction_validator.dtos.PaymentProcessingRecord;
+import com.transactionvalidator.transaction_validator.dtos.ValidatorResponse;
 import com.transactionvalidator.transaction_validator.producers.CreatePaymentProcess;
+import com.transactionvalidator.transaction_validator.utils.enums.PaymentStatus;
 
 @Service
 public class PaymentService {
-
     @Autowired
     private CreatePaymentProcess createPaymentProcess;
 
     @Autowired
     private PaymentRepository paymentRepository;
 
-    public List<Payment> getAllUsers() {
-        return paymentRepository.findAll();
-    }
-
     public Payment save(Payment payment) {
         Payment paymentt = paymentRepository.save(payment);
-        createPaymentProcess.setPayload(new PaymentRecord(paymentt.getId().toString(),
+        PaymentProcessingRecord paymentToSend = new PaymentProcessingRecord(
+        paymentt.getId().toString(),
+        paymentt.getId().toString(),
         paymentt.getCardnumber(),
         paymentt.getCvv(),
         paymentt.getAmount(),
         paymentt.getExpiredate(),
         paymentt.getCreatedate(),
-        paymentt.getStatuspayment().toString()));
+        paymentt.getStatuspayment().toString());
         try {
-            createPaymentProcess.sendPaymentProcess();
+            createPaymentProcess.sendPaymentProcess(paymentToSend);
         } catch (JsonProcessingException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return payment;
+    }
+
+    public void updatePaymentByValidatorResponse(ValidatorResponse status) {
+        Payment payment = paymentRepository.findById(UUID.fromString(status.paymentid())).get();
+        payment.setStatuspayment(PaymentStatus.valueOf(status.status()));
+        paymentRepository.save(payment);
     }
 }
